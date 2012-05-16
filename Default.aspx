6ad9b213bee56e574b2ebd4649d828bc0d0a2077
stefan.eydal <stefan.eydal@gmail.com>
2012-05-16 00:00:20 +0000

    importing project to google code

---
+++ Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" ValidateRequest="false" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script id="myalert" type="text/javascript"></script>
+
+</head>
+<body>
+<a href="http://weblogs.asp.net/scottgu/archive/2010/04/06/new-lt-gt-syntax-for-html-encoding-output-in-asp-net-4-and-asp-net-mvc-2.aspx">Þarf að skoða</a>
+    
+<form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="TextBox1" runat="server" Height="85px" TextMode="MultiLine" Width="347px"></asp:TextBox>
+        <%--<%:TextBox1.Text %>--%>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Button" onclick="Button1_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string encodedText = string.Empty;
+        //encodedText = Server.HtmlEncode(TextBox1.Text);
+        encodedText = HttpUtility.HtmlEncode(TextBox1.Text);
+
+        //System.Web.HttpContext.Current.Response.Write("<SCRIPT>alert(" + "test" + ")</SCRIPT>");
+
+        //ClientScript.RegisterStartupScript(this.GetType(), "myalert", "<SCRIPT>alert('" + "test" + "')</SCRIPT>;", true);
+        
+        //ClientScriptManager.RegisterStartupScript(this.GetType(), "myalert", "<SCRIPT>alert('" + "test" + "')</SCRIPT>;", true);
+        string str = HttpUtility.HtmlDecode(encodedText);
+        Response.Write("<script>alert('" + encodedText + "')</script>");
+
+        //Label1.Text = encodedText
+        Label1.Text = encodedText;
+
+        //Label1.Text = TextBox1.Text;
+    }
+}
